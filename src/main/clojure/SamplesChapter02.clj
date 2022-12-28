67f86ab5c39695755bf54ccef20d64d037160e05
fernando.avanzo <fernando.avanzo@gmail.com>
2022-12-28 16:16:43 -0300

    add comment of syntax session of the book

--- src/main/clojure/SamplesChapter02.clj
+++ src/main/clojure/SamplesChapter02.clj
@@ -10,3 +10,18 @@
   (> x 0) "Greater!"
   (= x 0) "zero!"
   (< x 0) "lesser!")
+
+(comment
+  (+ 1 2 3 4 5)
+
+  (+ 1, 2, 3, 4, 5)
+
+  (+ 1, 2, 3, 4, 5)
+
+  (+ 1,,,,, 2, 3 4,, 5)
+
+  ;case where the comma is used as a separator
+  (def a-map {:a 1 :b 2 :c 3})
+  (println a-map)
+  )
+
