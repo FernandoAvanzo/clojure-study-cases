b1506178a70b6706f8cf3870ab82cbc48f78e064
fernando.avanzo <fernando.avanzo@gmail.com>
2022-12-29 10:34:01 -0300

    add all book samples expression into a comment function

--- src/main/clojure/SamplesChapter02.clj
+++ src/main/clojure/SamplesChapter02.clj
@@ -1,17 +1,16 @@
 (ns SamplesChapter02)
 
-
-(def my-addition (fn [a b] (+ a b)))
-
-(println "hello world")
-
-(def x 1)
-(cond
-  (> x 0) "Greater!"
-  (= x 0) "zero!"
-  (< x 0) "lesser!")
-
 (comment
+  (def my-addition (fn [a b] (+ a b)))
+
+  (println "hello world")
+
+  (def x 1)
+  (cond
+    (> x 0) "Greater!"
+    (= x 0) "zero!"
+    (< x 0) "lesser!")
+
   (+ 1 2 3 4 5)
 
   (+ 1, 2, 3, 4, 5)
@@ -22,6 +21,5 @@
 
   ;case where the comma is used as a separator
   (def a-map {:a 1 :b 2 :c 3})
-  (println a-map)
-  )
+  (println a-map))
 
