e1a08859e60e8dfd6fa0aa1516409fc1557eb0b9
fernando.avanzo <fernando.avanzo@gmail.com>
2022-12-29 11:26:47 -0300

    java interop example

--- src/main/clojure/SamplesChapter02.clj
+++ src/main/clojure/SamplesChapter02.clj
@@ -5,12 +5,14 @@
 
   (println "hello world")
 
+  ;cond function used to evaluate a series of conditions
   (def x 1)
   (cond
     (> x 0) "Greater!"
     (= x 0) "zero!"
     (< x 0) "lesser!")
 
+  ;commas are optional
   (+ 1 2 3 4 5)
 
   (+ 1, 2, 3, 4, 5)
@@ -21,5 +23,9 @@
 
   ;case where the comma is used as a separator
   (def a-map {:a 1 :b 2 :c 3})
-  (println a-map))
+  (println a-map)
+
+  ;java interop example
+  (.contains "clojure-in-action" "-")
+  (.endsWith "program.clj" ".clj"))
 
